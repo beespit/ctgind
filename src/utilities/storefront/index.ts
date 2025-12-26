4b3b3157a348e020e56d58dfff8ce89fb3c44d28
half-nelson1230 <al@onliest.studio>
2025-12-26 12:01:52 -0800

    kickstarter shopify

--- src/utilities/storefront/index.ts
+++ src/utilities/storefront/index.ts
@@ -1,8 +1,8 @@
 import { createStorefrontClient } from '@shopify/hydrogen-react';
 
-export const storeDomain = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN as string;
-export const publicStorefrontToken = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_TOKEN as string;
-export const storefrontApiVersion = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_VERSION as string;
+export const storeDomain = process.env.SHOPIFY_STORE_DOMAIN!;
+export const publicStorefrontToken = process.env.SHOPIFY_STOREFRONT_API_TOKEN!;
+export const storefrontApiVersion = process.env.SHOPIFY_STOREFRONT_API_VERSION!;
 
 const { getStorefrontApiUrl, getPublicTokenHeaders } = createStorefrontClient({
   storeDomain,
